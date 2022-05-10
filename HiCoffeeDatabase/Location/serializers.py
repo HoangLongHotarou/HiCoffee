ec5ce20c98545a92cfbec676cafe680241534337
HoangLongHotarou <longhoangdazai@gmail.com>
2022-05-10 13:56:59 +0700

    update

--- HiCoffeeDatabase/Location/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import CoffeeShop
-
-
-class CoffeeShopSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CoffeeShop
-        fields = ('id', 'name', 'rate', 'image')
--- HiCoffeeDatabase/Location/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import CoffeeShop
-from .serializers import CoffeeShopSerializer
-# Create your views here.
-
-class CoffeeShopViewSet(ModelViewSet):
-    queryset = CoffeeShop.objects.all()
-    serializer_class = CoffeeShopSerializer
