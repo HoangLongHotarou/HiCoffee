334ecb6f90aa55d1099f64208dfccf1f14e1c5fd
NguyenKhoa-dev <S4xlamdong@gmail.com>
2022-05-13 21:35:13 +0700

    Cua Khoa ne

--- HiCoffee/src/app/components/components.module.ts
+++ HiCoffee/src/app/components/components.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TabsComponent } from './tabs/tabs.component';
 
 const COMPONENTS = [
-    TabsComponent,
+
 ]
 
 @NgModule({
   declarations: COMPONENTS,
   exports: COMPONENTS,  
   imports: [
-    CommonModule
+    CommonModule,    
   ] 
 })
 export class ComponentsModule {}
