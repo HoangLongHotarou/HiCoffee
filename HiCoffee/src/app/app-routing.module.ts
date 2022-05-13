bd41c50c1069bc11b2a882456d8e5b1bb9cef86f
NguyenKhoa-dev <S4xlamdong@gmail.com>
2022-05-13 21:59:36 +0700

    Khoa update

--- HiCoffee/src/app/app-routing.module.ts
+++ HiCoffee/src/app/app-routing.module.ts
@@ -23,9 +23,10 @@ const routes: Routes = [
     path: 'tabs',
     loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
+  {
     path: 'forgetpassword',
     loadChildren: () => import('./pages/forgetpassword/forgetpassword.module').then( m => m.ForgetpasswordPageModule)
-  },
+  }
 ];
 
 @NgModule({
--- HiCoffee/src/app/components/components.module.ts
+++ HiCoffee/src/app/components/components.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TabsComponent } from './tabs/tabs.component';
 import { InputComponent } from './input/input.component';
 import { IonicModule } from '@ionic/angular';
 import { CardComponent } from './card/card.component';
 
 const COMPONENTS = [
-    TabsComponent,
     InputComponent,
     CardComponent
 ]
