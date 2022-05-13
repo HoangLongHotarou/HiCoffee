9c19235c34298bc4268c8359140e8647e84430b9
unknown <vocongly05042001@gmail.com>
2022-05-13 16:13:00 +0700

    Ly push Login Signup UI

--- HiCoffee/src/app/components/components.module.ts
+++ HiCoffee/src/app/components/components.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabsComponent } from './tabs/tabs.component';
+import { InputComponent } from './input/input.component';
+import { IonicModule } from '@ionic/angular';
 
 const COMPONENTS = [
     TabsComponent,
+    InputComponent
 ]
 
 @NgModule({
   declarations: COMPONENTS,
   exports: COMPONENTS,  
   imports: [
-    CommonModule
+    CommonModule,IonicModule
   ] 
 })
 export class ComponentsModule {}
