d7c90093bbc6d98c6f9f864b0e1c43387eb09276
NguyenKhoa-dev <S4xlamdong@gmail.com>
2022-05-20 21:29:38 +0700

    Khoa push cai nhe

--- HiCoffee/src/app/components/slidecategory/slidecategory.component.ts
+++ HiCoffee/src/app/components/slidecategory/slidecategory.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { CoffeeShop } from 'src/app/interfaces/coffeeshop';
 import { FetchAPIService } from 'src/app/services/fetch-api.service';
@@ -17,15 +17,22 @@ export class SlidecategoryComponent implements OnInit {
 
   //coffeeShop$: CoffeeShop[] = [];
 
-  constructor(private fetchAPI: FetchAPIService,private router: Router) { }
+  width: number = window.innerWidth;
+  slidePerView: number = 1;
+  cafeSlideOpts;
 
-  // eslint-disable-next-line @typescript-eslint/member-ordering
-  cafeSlideOpts = {
-    freeMode : true,
-    slidesPerView: 2,
-    slidesOffsetBefore: 11,
-    spaceBetween: 10
-  };
+  constructor(private fetchAPI: FetchAPIService,private router: Router) {
+    this.slidePerView = (this.width >= 768) ? 3 : 2; 
+    this.cafeSlideOpts = {
+      slidesPerView: this.slidePerView,
+      centeredSlides: true,
+      autoplay: true,
+      loop: true,
+      spaceBetween: 20
+    };  
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering  
 
   ngOnInit() {
     // this.fetchAPI.findAll('location/coffeeshops/?page=2').then((res) => {
