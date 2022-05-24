1daec1bdb437e8a07a6ef8c016d462a1a83c6c5c
HoangLongHotarou <longhoangdazai@gmail.com>
2022-05-24 16:45:34 +0700

    main

--- HiCoffee/src/app/app-routing.module.ts
+++ HiCoffee/src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule),
-    // canLoad: [AutoLoginGuard]
+    canLoad: [AutoLoginGuard]
   },
   {
     path: 'signup',
@@ -23,7 +23,7 @@ const routes: Routes = [
   {
     path: 'introduce',
     loadChildren: () => import('./pages/introduce/introduce.module').then(m => m.IntroducePageModule),
-    // canLoad: [AuthGuard]
+    canLoad: [AutoLoginGuard]
   },
   {
     path: 'tabs',
